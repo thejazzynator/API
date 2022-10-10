880a7c92914bd48842bf1de9a905239c81c07f12
Jasmine Graham <thejazzynator@gmail.com>
2022-10-09 20:45:22 -0400

    more updates

---
+++ API/SwaggerConfig.cs
@@ -0,0 +1,16 @@
+﻿using Swashbuckle.Application;
+using System.Web.Http;
+
+namespace MusicAPI
+{
+    public class SwaggerConfig
+    {
+        public static void Register()
+        {
+            var thisAssembly = typeof(SwaggerConfig).Assembly;
+            GlobalConfiguration.Configuration
+              .EnableSwagger(c => c.SingleApiVersion("v1", "ErrorHandlingWebAPI"))
+              .EnableSwaggerUi();
+        }
+    }
+}
